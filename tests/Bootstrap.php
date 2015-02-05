07b89332b32a52347ad9309be0e400f20b34bfb3
Jens Kohl <jens.kohl@gmail.com>
2015-02-05 17:36:21 +0100

    State SAPI information while running phpunit

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -7,6 +7,13 @@ chdir(__DIR__);
 // complains if it is not set in 'date.timezone' of php.ini.
 date_default_timezone_set('UTC');
 
+echo PHP_EOL;
+echo "Value of PHP_SAPI: " . PHP_SAPI . PHP_EOL;
+echo "Value of php_sapi_name(): " . php_sapi_name() . PHP_EOL;
+echo '$_SERVER[\'HTTP_USER_AGENT\'] is set: ' . (isset($_SERVER['HTTP_USER_AGENT']) ? 'true' : 'false') . PHP_EOL;
+echo PHP_EOL;
+
+
 // Ensure that composer has installed all dependencies
 if (!file_exists(dirname(__DIR__) . '/composer.lock')) {
     die("Dependencies must be installed using composer:\n\nphp composer.phar install --dev\n\n"
