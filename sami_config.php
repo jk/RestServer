473ec1e2eefb2a38462556fa110a75af3fab0446
Jens Kohl <jens.kohl@gmail.com>
2015-03-24 15:42:09 +0100

    Make sami/sami config PSR1/2 compliant

--- sami_config.php
+++ sami_config.php
@@ -8,20 +8,20 @@ $dir = __DIR__ . '/src';
 $iterator = Finder::create()
     ->files()
     ->name('*.php')
-    ->in($dir)
-;
+    ->in($dir);
 
 $versions = GitVersionCollection::create($dir)
-    ->addFromTags(function ($version) { return preg_match('/^v?\d+\.\d+\.\d+$/', $version); })
+    ->addFromTags(function ($version) {
+        return preg_match('/^v?\d+\.\d+\.\d+$/', $version);
+    })
     ->add('master', 'production')
-    ->add('develop', 'development')
-;
+    ->add('develop', 'development');
 
 $options = array(
-    'title'                => 'jk/restserver API',
-    'versions'             => $versions,
-    'build_dir'            => __DIR__.'/build/sami/%version%',
-    'cache_dir'            => __DIR__.'/build/sami_cache/%version%',
+    'title' => 'jk/restserver API',
+    'versions' => $versions,
+    'build_dir' => __DIR__ . '/build/sami/%version%',
+    'cache_dir' => __DIR__ . '/build/sami_cache/%version%',
     'default_opened_level' => 2,
 );
 
